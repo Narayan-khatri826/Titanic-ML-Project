afe5044021e1108bba9e3c6fdf430ecd849e9816
Narayan-khatri826 <narayan.23ece305@rtu.ac.in>
2026-05-21 21:24:58 +0530

    Fixed app

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ model = pickle.load(open("model.pkl", "rb"))
 
 st.title("Titanic Survival Prediction")
 
+passengerid = st.number_input("Passenger ID")
+
 pclass = st.number_input("Passenger Class")
 
 sex = st.number_input("Sex (0=Male, 1=Female)")
@@ -22,15 +24,15 @@ parch = st.number_input("Parents/Children")
 
 fare = st.number_input("Fare")
 
+embarked = st.number_input("Embarked (0=C, 1=Q, 2=S)")
+
+
+
 if st.button("Predict"):
     data = np.array([[pclass, sex, age, sibsp, parch, fare]])
-
-prediction = model.predict(data)
-
-if prediction[0] == 1:
-
-    st.success("Passenger Survived")
-
-else:
-      st.error("Passenger Did Not Survive")
+    prediction = model.predict(data)
+    if prediction[0] == 1:
+        st.success("Passenger Survived")
+    else:
+        st.error("Passenger Did Not Survive")
 
